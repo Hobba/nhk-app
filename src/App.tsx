0bc54d1a1ada60ae4bfd72ab5c74b966d3ad3cc2
Robin <hobba11@msn.com>
2023-06-06 23:10:33 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './nerdhalbkugel-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hier entsteht die neue Nerdhalkbugel Website / App.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
