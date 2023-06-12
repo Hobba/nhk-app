067b24b7eaf5da0d1243fbd4a6af114bdaf92da8
Robin <hobba11@msn.com>
2023-06-12 21:43:05 +0200

    Adjusted margin

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" />
-        <p>
-          Hier entsteht die neue Nerdhalkbugel Website.
+         <p>
+            Hier entsteht die neue Nerdhalkbugel Website.
         </p>
+        <img src={logo} className="App-logo" />
         <div>
-        <iframe src="https://open.spotify.com/embed/show/0izNTbydZKIgt1oY1p63vz?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe className="Spotify-widget" src="https://open.spotify.com/embed/show/0izNTbydZKIgt1oY1p63vz?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
       </header>
     </div>
