1bda22a292c45f267fe614cee66d67c5e609179e
Robin <hobba11@msn.com>
2023-06-12 21:19:10 +0200

    Added background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import background from './background.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" />
         <p>
-          Hier entsteht die neue Nerdhalkbugel Website / App.
+          Hier entsteht die neue Nerdhalkbugel Website.
         </p>
         <div>
         <iframe src="https://open.spotify.com/embed/show/0izNTbydZKIgt1oY1p63vz?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
