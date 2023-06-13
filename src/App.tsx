eb889b349ef8e7dd6348bbd47e4cf646989a7f1b
Robin <hobba11@msn.com>
2023-06-14 00:40:17 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,48 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 import background from './background.jpg'
+import Button from '@mui/material/Button';
+import FullWidthTabs from './Navigation/FullWidthTabs'
+import { LinkTab, LinkTabProps, handleChange }from './Navigation/NavTab'
+import AboutUs from './Pages/AboutUs'
+import Episodes from './Pages/Episodes'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-         <p>
-            Hier entsteht die neue Nerdhalkbugel Website.
-        </p>
-        <img src={logo} className="App-logo" />
-        <div>
-            <iframe className="Spotify-widget" src="https://open.spotify.com/embed/show/0izNTbydZKIgt1oY1p63vz?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-      </header>
-    </div>
+
+  <Router>
+        <Box sx={{ width: '100%' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
+            <LinkTab label="About Us" to="/aboutus" />
+            <LinkTab label="Episodes" to="/episodes" />
+            <LinkTab label="Page Three" to="/pagethree" />
+          </Tabs>
+        </Box>
+
+        <Routes>
+          <Route path="/" element={<AboutUs />} />
+          <Route path="/episodes" element={<Episodes />} />
+          {/* Add other routes for your application */}
+        </Routes>
+      </Router>
+//     <div className="App">
+//     <NavTab />
+//       <header className="App-header">
+//          <p>
+//             Hier entsteht die neue Nerdhalkbugel Website.
+//         </p>
+//         <img src={logo} className="App-logo" />
+//         <div>
+//             <iframe className="Spotify-widget" src="https://open.spotify.com/embed/show/0izNTbydZKIgt1oY1p63vz?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+//
+//
+//       </header>
+//     </div>
   );
 }
 
---
+++ src/Pages/Episodes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Episodes = () => {
+  return (
+    <div>
+      <h1>Episodes</h1>
+      {/* Add your page content here */}
+    </div>
+  );
+};
+
+export default Episodes;
