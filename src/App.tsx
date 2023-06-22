b9b40afa6abae56f0ff7730d0aad5cbb54812f11
Hobba <hobba11@msn.com>
2023-06-22 18:31:27 +0200

    Activate vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-import background from './background.jpg'
+import {inject} from "@vercel/analytics";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-         <p>
-            Hier entsteht die neue Nerdhalkbugel Website.
-        </p>
-        <img src={logo} className="App-logo" />
-        <div>
-            <iframe className="Spotify-widget" src="https://open.spotify.com/embed/show/0izNTbydZKIgt1oY1p63vz?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    inject();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Hier entsteht die neue Nerdhalkbugel Website.
+                </p>
+                <img src={logo} className="App-logo"/>
+                <div>
+                    <iframe className="Spotify-widget"
+                            src="https://open.spotify.com/embed/show/0izNTbydZKIgt1oY1p63vz?utm_source=generator"
+                            width="100%" height="352" frameBorder="0"
+                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                            loading="lazy"></iframe>
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
