357e5f29618e143c83500281fc2b20ef16cb144b
Robin, BöckmanntName <hobba11@msn.com>
2023-07-14 20:21:52 +0200

    Basic app implementation

---
+++ src/pages/Episodes/Episodes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Episodes = () => {
+    return (
+        <body className="body">
+            <h1>Episoden</h1>
+            <div className="placeholder-text">
+                <h2>Hier können demnächst alle unsere Podcastfolgen gehört werden!</h2>
+            </div>
+        </body>
+    );
+};
+
+export default Episodes;
