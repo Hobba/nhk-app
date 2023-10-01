fbcababc779eb31649648fe2c8729bf7128240a5
robin.boeckmann <robin.boeckmann@flixbus.com>
2023-10-01 20:31:53 +0200

    Modify timeline legend

--- src/pages/Extras/Extras.tsx
+++ src/pages/Extras/Extras.tsx
@@ -4,27 +4,65 @@ import AltRouteIcon from '@mui/icons-material/AltRoute';
 import SeriesIcon from "../../components/Icons/Logo_Serie.png";
 import MovieIcon from "../../components/Icons/Logo_Film.png"
 import OneShotIcon from '../../components/Icons/Logo_OneShot.png';
-import {Icon} from "@mui/material";
+import {createTheme, Icon, ThemeProvider} from "@mui/material";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 
 const Extras = () => {
-    return (
-        <div className="page">
-            <h2 className="headline2">Marvel Cinematic Universe Timeline</h2>
-            <div className="legend">
-                <Icon>
-                    <img src={MovieIcon} height={25} width={25} alt={MovieIcon}/>
-                </Icon> <span className="extra-icon">Film</span>
-                <Icon>
-                    <img src={SeriesIcon} height={25} width={25} alt={SeriesIcon}/>
-                </Icon> <span className="extra-icon">Serie</span>
-                <Icon>
-                    <img src={OneShotIcon} height={25} width={25} alt={OneShotIcon}/>
-                </Icon> <span className="extra-icon">One-Shot</span>
-                <AltRouteIcon></AltRouteIcon> <span className="extra-icon">Alternative Timeline</span>
-            </div>
 
-            <CustomizedTimeline/>
-        </div>
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#50139d',
+            },
+            secondary: {
+                main: '#e1ce1b'
+            },
+            warning: {
+                main: '#ed1d24'
+            },
+            info: {
+                main: '#ffffff'
+            },
+            success: {
+                main: '#3399ff'
+            },
+            text: {
+                primary: '#ffffff',
+            }
+        },
+    });
+
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="page">
+                <h2 className="headline2">Marvel Cinematic Universe Timeline</h2>
+                <Box sx={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    zIndex: 1,
+                    width: '95%',
+                    paddingBottom: '10px'
+                }}>
+                    <Paper elevation={5} sx={{justifyContent: "center", opacity: 0.7, backgroundColor: 'black'}}>
+                        <div className="legend">
+                            <Icon>
+                                <img src={MovieIcon} height={25} width={25} alt={MovieIcon}/>
+                            </Icon> <span className="movie-text">Film</span>
+                            <Icon>
+                                <img src={SeriesIcon} height={25} width={25} alt={SeriesIcon}/>
+                            </Icon> <span className="series-text">Serie</span>
+                            <Icon>
+                                <img src={OneShotIcon} height={25} width={25} alt={OneShotIcon}/>
+                            </Icon> <span className="oneshot-text">One-Shot</span>
+                            <AltRouteIcon color='info'></AltRouteIcon> <span
+                            className="alt-timeline-text">Alternative Zeitlinie</span>
+                        </div>
+                    </Paper>
+                </Box>
+                <CustomizedTimeline/>
+            </div>
+        </ThemeProvider>
     );
 };
 
