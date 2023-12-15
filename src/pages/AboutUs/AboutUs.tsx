86dcbff06b42d357eac4b34319d96c3f93f256c4
robin.boeckmann <robin.boeckmann@flixbus.com>
2023-12-15 11:57:16 +0100

    Add push test button

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -5,12 +5,14 @@ const AboutUs = () => {
     return (
         <div className="page">
             <h1 className="headline">Über Uns</h1>
-            <Button variant="contained" color="primary" onClick={handleClick}>Sende eine Sync Message!</Button>
+            <Button variant="contained" color="primary" onClick={sendSyncMessage}>Sende eine Sync Message!</Button>
+            <br/>
+            <Button variant="contained" color="primary" onClick={sendPushNotification}>Sende eine Push Notification!</Button>
         </div>
     );
 };
 
-const handleClick = () => {
+const sendSyncMessage = () => {
     console.log("Button geklickt.")
     if ('serviceWorker' in navigator && 'SyncManager' in window) {
         navigator.serviceWorker.ready.then((registration) => {
@@ -20,5 +22,17 @@ const handleClick = () => {
         // Mach die Aktion ohne "Umwege".
     }
 };
+
+const sendPushNotification = () => {
+    const title = 'Notification aus dem Code';
+    const options = {
+        body: 'Dies ist eine Notification, die im Code definiert ist.',
+    };
+    navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification(title, options)
+            .then(r => console.log("Sent"))
+            .catch(e => console.log(e));
+    });
+}
 
 export default AboutUs;
